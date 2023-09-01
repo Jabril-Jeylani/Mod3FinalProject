bb4c3061d7aba6cc985d6492890e0662d12071fc
Jabril-Jeylani <jabrilmj@gmail.com>
2023-09-01 00:04:16 -0600

    working on backend

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const PORT = 8080 
+const connectDB = require('./config')
+connectDB()
+app.use(express.json())
+app.use(cors())
+
+app.listen(PORT, () => {
+    console.log('Listening to the port: ' + PORT)
+})
