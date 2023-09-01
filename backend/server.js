17a0bcace383d4bde73d6318a2c7f1b30f3aae7d
Jabril-Jeylani <jabrilmj@gmail.com>
2023-09-01 02:13:00 -0600

    before connecting both ends

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const data = require('./data')
 const express = require('express')
 const cors = require('cors')
 const app = express()
@@ -7,6 +8,10 @@ const connectDB = require('./config')
 connectDB()
 app.use(express.json())
 app.use(cors())
+
+app.get('/api/products', (req, res) => {
+    res.send(data.products)
+})
 
 app.listen(PORT, () => {
     console.log('Listening to the port: ' + PORT)
