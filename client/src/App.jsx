c74cb4e12ebc09e447532b8a70d72952d6739608
Jabril-Jeylani <jabrilmj@gmail.com>
2023-09-06 16:29:32 -0600

    react bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,21 +4,42 @@ import "./App.css";
 import Home from "./pages/Home";
 import { Link, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
-
 	return (
-		<div>
+		<div className="d-flex flex-column site-container">
 			<header>
-				<Link to="/">Pet Shop</Link>
+				<Navbar
+					bg="dark"
+					variant="dark"
+				>
+					<Container>
+						<LinkContainer to="/">
+							<Navbar.Brand>Pet Shop</Navbar.Brand>
+						</LinkContainer>
+					</Container>
+				</Navbar>
 			</header>
 			<main>
-        <Routes>
-          <Route path="product/:slug" element={<Product />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-				
+				<Container>
+					<Routes>
+						<Route
+							path="product/:slug"
+							element={<Product />}
+						/>
+						<Route
+							path="/"
+							element={<Home />}
+						/>
+					</Routes>
+				</Container>
 			</main>
+			<footer>
+				<div className='tex-center'>All rights reserved</div>
+			</footer>
 		</div>
 	);
 }
