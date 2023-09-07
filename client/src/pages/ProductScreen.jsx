51654d094017eea665b6e37f844d5b761a1ec08a
Jabril-Jeylani <jabrilmj@gmail.com>
2023-09-06 18:39:46 -0600

    added spinner

--- client/src/pages/ProductScreen.jsx
+++ client/src/pages/ProductScreen.jsx
@@ -9,6 +9,8 @@ import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
 import Rating from '../components/Rating';
 import { Helmet } from 'react-helmet-async';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -22,6 +24,12 @@ const reducer = (state, action) => {
       return state;
   }
 };
+
+const getError = (error) => {
+    return error.response && error.response.data.message
+    ? error.response.data.message
+    : error.message
+}
 
 function ProductScreen() {
   const params = useParams();
@@ -39,17 +47,17 @@ function ProductScreen() {
         const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
       }
     };
     fetchData();
   }, [slug]);
 
   return loading ? (
-    <div>Loading...</div>
+    <LoadingBox />
   ) : error ? (
-    <div>{error}</div>
-  ) : (
+    <MessageBox variant="danger">{error}</MessageBox>
+  )  : (
     <div>
       <Row>
         <Col md={6}>
