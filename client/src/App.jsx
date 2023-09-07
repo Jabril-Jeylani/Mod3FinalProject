ab1cd53220fc0d17f582aeb2708541edb0d19bdf
Jabril-Jeylani <jabrilmj@gmail.com>
2023-09-06 18:57:17 -0600

    useContext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
-import { useEffect, useState } from "react";
-import data from "./data";
 import "./App.css";
-import Home from "./pages/HomeScreen";
 import { Link, Route, Routes } from "react-router-dom";
-import Product from "./components/Product";
 import Navbar from "react-bootstrap/Navbar";
+import Badge from "react-bootstrap/Badge";
+import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import { LinkContainer } from "react-router-bootstrap";
 import HomeScreen from "./pages/HomeScreen";
 import ProductScreen from "./pages/ProductScreen";
+import { useContext } from "react";
+import { Store } from "./Store";
 
 function App() {
+	const { state } = useContext(Store);
+	const { cart } = state
+
 	return (
 		<div className="d-flex flex-column site-container">
 			<header>
@@ -22,6 +25,16 @@ function App() {
 						<LinkContainer to="/">
 							<Navbar.Brand>Pet Shop</Navbar.Brand>
 						</LinkContainer>
+						<Nav className="me-auto">
+							<Link to="/cart" className="nav-link">
+								Cart
+								{cart.cartItems.length > 0 && (
+									<Badge pill bg ="danger">
+										{cart.cartItems.length}
+									</Badge>
+								)}
+							</Link>
+						</Nav>
 					</Container>
 				</Navbar>
 			</header>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HelmetProvider } from 'react-helmet-async'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from "react-router-dom";
+import { StoreProvider } from "./Store.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
-    <HelmetProvider>
-    <App />
-    </HelmetProvider>
-    
-  </Router>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<Router>
+		<StoreProvider>
+			<HelmetProvider>
+				<App />
+			</HelmetProvider>
+		</StoreProvider>
+	</Router>
+);
