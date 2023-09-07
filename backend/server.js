674a25496cb170f2e2390cb56e09ac3b865eeae7
Jabril-Jeylani <jabrilmj@gmail.com>
2023-09-07 00:12:40 -0600

    user and auth added

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,19 @@ const connectDB = require('./config')
 connectDB()
 const seedRoutes = require('./routes/seedRoutes')
 const productRoutes = require('./routes/productRoutes')
+const authRoutes = require('./routes/authRoutes')
+const userRoutes = require('./routes/userRoutes')
+
+const { authorize } = require('./middleware/authMiddleware')
+
 app.use(express.json())
 app.use(cors())
 
 app.use('/api/seed', seedRoutes )
 app.use('/api/products', productRoutes)
+app.use('/api/users', authorize, userRoutes)
+app.use('/auth', authRoutes)
+
 
 
 app.listen(PORT, () => {
